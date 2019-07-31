3401776b9d24a46b821442b98a327bfbe7b977b9
Mike Li <limike@limike-macbookpro.roam.corp.google.com>
2019-07-31 20:56:34 +0800

    Add below usage in rmscaculator usage: rmscalculator.py [file 1] [period start] [period end] [file 2] [period start] [period end]

--- rmscalculator.py
+++ rmscalculator.py
@@ -42,6 +42,8 @@ def dbCalculator(signal, sample_rate):
 def showUsage():
     print('usage: rmscalculator.py [file] [1st period start] [1st period end] [2nd period start] [2nd period end]')
     print('example: rmscalculator.py test.wav 1 5 11 15')
+    print('usage: rmscalculator.py [file 1] [period start] [period end] [file 2] [period start] [period end]')
+    print('example: rmscalculator.py test1.wav 1 5 test2.wav 11 15')
 
 def dbCalculatorHelper(filename, t1, t2, t3, t4):
     if (t1 > t2 or t3 > t4):
@@ -76,21 +78,76 @@ def dbCalculatorHelper(filename, t1, t2, t3, t4):
         # place a text box in upper left in axes coords
         ax.text(0.05, 0.95 - 0.24*i, textstr, transform=ax.transAxes, fontsize=14,
             verticalalignment='top', bbox=props)
+    plt.show()
 
+def dbCalculatorHelper2(filename1, filename2, t1, t2, t3, t4):
+    if (t1 > t2 or t3 > t4):
+        showUsage()
+        return
+    [signal, sample_rate, channels] = audioBasicIO.readAudioFile(filename1)
+    [signal2, sample_rate2, channels2] = audioBasicIO.readAudioFile(filename2)
+    print("shape = {}, dim = {}, sample_rate = {} channels = {}".format(signal.shape, signal.shape[1], sample_rate, channels))
+    sampleNum = signal.shape[1]
+    trackLen = sampleNum/sample_rate
+    print('track1 length = %.2f seconds' % trackLen)
+
+    trackLen2 = signal2.shape[1]/sample_rate2
+    print('track2 length = %.2f seconds' % trackLen2)
+
+    if (t2 > trackLen or t4 > trackLen2):
+        showUsage()
+        return
+    if (channels > 4 or channels2 > 4):
+        print('audio file channels should be <= 4')
+        return
+    if (channels != channels2):
+        print('audio file channel number not match')
+        return
+
+    fig, ax = plt.subplots()
+    fig.suptitle('Test Result', fontsize=14, fontweight='bold')
+    # these are matplotlib.patch.Patch properties
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+
+    for i in range(channels):
+        db1 = dbCalculator(signal[i][t1*sample_rate : t2*sample_rate], sample_rate)
+        db2 = dbCalculator(signal2[i][t3*sample_rate2 : t4*sample_rate2], sample_rate2)
+
+        textstr = '\n'.join((
+        r'1st period = %.4f dB' % (db1, ),
+        r'2nd period = %.4f dB' % (db2, ),
+        r'difference = %.4f dB' % (abs(db1 - db2), )))
+        # place a text box in upper left in axes coords
+        ax.text(0.05, 0.95 - 0.24*i, textstr, transform=ax.transAxes, fontsize=14,
+            verticalalignment='top', bbox=props)
     plt.show()
 
 def main():
-    if (len(sys.argv) < 6):
+
+    if(len(sys.argv) == 6):
+        filename = sys.argv[1]
+        t1 = int(sys.argv[2])
+        t2 = int(sys.argv[3])
+        t3 = int(sys.argv[4])
+        t4 = int(sys.argv[5])
+        print('filename = ' + filename)
+        print("t1 = {}, t2 = {}, t3 = {} t4 = {}".format(t1, t2, t3, t4))
+        dbCalculatorHelper(filename, t1, t2, t3, t4)
+    elif(len(sys.argv) == 7):
+        filename1 = sys.argv[1]
+        t1 = int(sys.argv[2])
+        t2 = int(sys.argv[3])
+        filename2 = sys.argv[4]
+        t3 = int(sys.argv[5])
+        t4 = int(sys.argv[6])
+        print('filename1 = ' + filename1)
+        print("t1 = {}, t2 = {}".format(t1, t2))
+        print('filename2 = ' + filename2)
+        print("t3 = {} t4 = {}".format(t3, t4))
+        dbCalculatorHelper2(filename1, filename2, t1, t2, t3, t4)
+    else:
         showUsage()
         sys.exit()
-    filename = sys.argv[1]
-    t1 = int(sys.argv[2])
-    t2 = int(sys.argv[3])
-    t3 = int(sys.argv[4])
-    t4 = int(sys.argv[5])
-    print('filename = ' + filename)
-    print("t1 = {}, t2 = {}, t3 = {} t4 = {}".format(t1, t2, t3, t4))
-    dbCalculatorHelper(filename, t1, t2, t3, t4)
 
 if __name__== "__main__":
   main()
